13fc50c1b239e5635132873384a327b68cd5a318
FrancescaPasse <fp2jf@virgini.edu>
2022-04-08 00:35:30 -0400

    PrimeNumber answer

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -2,18 +2,30 @@ package math_problems;
 
 public class PrimeNumber {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to print a list of all prime numbers from 2 to 1,000,000.
-     *  Print out the prime numbers in the given range.
-     *
+     * Print out the prime numbers in the given range.
+     * <p>
      * BONUS: Figure out how to improve algorithmic efficiency
      */
 
 
     public static void main(String[] args) {
 
+        for (int num = 2; num < 1000000; num++) {
+            boolean isPrime = true;
+            for (int p = 2; p < num; p++) {
+                if (num % p == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        if (isPrime==true){
+            System.out.print(num + " ");
+        }
+        }
 
 
     }
-
 }

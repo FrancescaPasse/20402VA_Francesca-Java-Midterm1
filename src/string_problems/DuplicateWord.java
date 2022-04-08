dbe5e54d4bd0d145987d76d225924ae676e43620
FrancescaPasse <fp2jf@virgini.edu>
2022-04-07 23:21:31 -0400

    FindLowestDifference answer

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -14,18 +14,6 @@ public class DuplicateWord {
     public static void main(String[] args) {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
-        // Implement here
-     st = st.toLowerCase();
-     String[]strArray=st.split("");
-        List<String> duplicateWords =new ArrayList<>();
-        HashSet< String> nonRepetitiveWords = new HashSet<>();
-        for (String str: strArray)
-
-        {
-            if (nonRepetitiveWords.add(st))
-                duplicateWords.add(st);
-        }
-     System.out.println(duplicateWords);
 
     }
 }

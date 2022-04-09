3f3d3f2eef8da7ed2b06d0602ef605e4d3db1baa
FrancescaPasse <fp2jf@virgini.edu>
2022-04-08 23:57:04 -0400

    permutation

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -8,10 +8,41 @@ public class Permutation {
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
-    public static void main(String[] args) {
+    public static String swapString(String a, int i, int j) {
+        char[] b = a.toCharArray();
+        char ch;
+        ch = b[i];
+        b[i] = b[j];
+        b[j] = ch;
 
-
+        return String.valueOf(b);
 
     }
+    public static void main(String[] args) {
+        String tr = "ABC";
+        int len = tr.length();
+        System.out.print("ABC= ");
 
+        generatePermutation(tr, 0, len);
+    }
+    private static void generatePermutation(String tr, int start, int end) {
+
+        if (start == end - 1)
+            System.out.print(tr);
+
+        else {
+            for (int i = start; i < end; i++) {
+                tr = swapString(tr, start, i);
+
+                generatePermutation(tr, start + 1, end);
+
+                tr = swapString(tr, start, i);
+            }
+        }
+    }
 }
+
+
+
+
+
